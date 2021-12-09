f3a4c33bafaaca1244ea0c6934d0e1b5d5870332
Adoleyine <adoleyineprince2001@gmail.com>
2021-12-09 16:55:06 +0000

    venv

---
+++ venv/area_and_perimeter_of_rectangle.py
@@ -0,0 +1,6 @@
+w = float(input("PLEASE ENTER THE WIDTH OF THE RECTANGLE (IN METRES): "))
+h = float(input("PLEASE ENTER THE HEIGHT OF THE RECTANGLE (IN METRES): "))
+perimeter = 2 * w + 2 * h
+area = w * h
+print("THE PERIMETER OF THE RECTANGLE IS ", perimeter)
+print("THE AREA OF THE RECTANGLE IS ", area)
---
+++ venv/area_of_triangle.py
@@ -0,0 +1,5 @@
+h = float(
+    input("PLEASE ENTER THE PERPENDICULAR HEIGHT OF THE TRIANGLE (IN METRES): "))
+b = float(input("PLEASE ENTER THE BASE OF THE TRIANGLE (IN METRES): "))
+area = 0.5 * h * b
+print("THE AREA OF THE TRIANGLE IS ", area)
