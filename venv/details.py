1009c6fdc9413acc1d2bcfcaed72e30912589749
Adoleyine <adoleyineprince2001@gmail.com>
2021-12-09 16:42:50 +0000

    VENV

---
+++ venv/details.py
@@ -0,0 +1,8 @@
+name = input("PLEASE ENTER YOUR NAME: ")
+age = input("PLEASE ENTER YOUR AGE: ")
+address = input("PLEASE ENTER YOUR ADDRESS: ")
+phone_number = input("PLEASE ENTER YOUR PHONE NUMBER: ")
+print("Hello ", name)
+print("You live in ", address)
+print("Your contact number is ", phone_number)
+print("You are ", age)
